2cfc67d6f720d770cb99a4ef42abadffd38d26a3
M-Farag <mina.farag@icloud.com>
2021-12-30 17:37:34 +0200

    ADD: Multiple assertions and assertion messages

--- test_calculator.py
+++ test_calculator.py
@@ -13,7 +13,9 @@ def test_add_integer_to_string_return_none():
 
 # Subtract
 def test_subtract_two_integers_return_integer():
-    assert cal.subtract(2,1) == 1
+    results = cal.subtract(2,1)
+    assert isinstance(results,int),"{} is not an integer".format(results)
+    assert  results == 1
 
 def test_subtract_two_strings_return_none():
     assert cal.subtract('a','b') is None
@@ -25,12 +27,16 @@ def test_subtract_two_integers_can_return_negative_value():
     assert cal.subtract(2,3) == -1
 
 def test_subtract_float_and_integer_can_return_float():
-    assert cal.subtract(2.0, 1) == 1.0
+    result = cal.subtract(2.0, 1)
+    assert isinstance(result,float), "{} is not a float".format(result)
+    assert result == 1.0
 
 # multiplication
 def test_multiply_two_integers_return_expected_value():
     assert cal.multiply(2,2) == 4
+
 def test_multiply_integer_to_string_return_none():
     assert cal.multiply(1,'m') is None
+    
 def test_multiply_two_strings_return_none():
     assert cal.multiply('m','i') is None
