e73716b170db83e1223fd01cea8eab044ad204bd
M-Farag <mina.farag@icloud.com>
2022-01-03 15:28:27 +0200

    UPD: Test module to organize unit tests in Testing Classes

--- test_calculator.py
+++ test_calculator.py
@@ -1,45 +1,49 @@
 import pytest
 import calculator as cal
 
-# ADD
-def test_add_two_numbers():
-    assert cal.add(1,1) == 2
 
-def test_add_two_strings_return_exception_type_error():
-    with pytest.raises(TypeError) as exception_info:
-        cal.add('a','b')
+class TestAdd(object):
+    # ADD
+    def test_add_two_numbers(self):
+        assert cal.add(1,1) == 2
 
-def test_add_integer_to_string_return_none():
-    with pytest.raises(TypeError) as exception_info:
-        cal.add(1,'a')
-    assert exception_info.match('You cannot add strings')
+    def test_add_two_strings_return_exception_type_error(self):
+        with pytest.raises(TypeError) as exception_info:
+            cal.add('a','b')
+
+    def test_add_integer_to_string_return_none(self):
+        with pytest.raises(TypeError) as exception_info:
+            cal.add(1,'a')
+        assert exception_info.match('You cannot add strings')
 
 # Subtract
-def test_subtract_two_integers_return_integer():
-    results = cal.subtract(2,1)
-    assert isinstance(results,int),"{} is not an integer".format(results)
-    assert  results == 1
+class TestSubtract(object):
+    def test_subtract_two_integers_return_integer(self):
+        results = cal.subtract(2,1)
+        assert isinstance(results,int),"{} is not an integer".format(results)
+        assert  results == 1
 
-def test_subtract_two_strings_return_none():
-    assert cal.subtract('a','b') is None
+    def test_subtract_two_strings_return_none(self):
+        assert cal.subtract('a','b') is None
 
-def test_subtract_integer_and_string_return_none():
-    assert cal.subtract(1,'n') is None
+    def test_subtract_integer_and_string_return_none(self):
+        assert cal.subtract(1,'n') is None
 
-def test_subtract_two_integers_can_return_negative_value():
-    assert cal.subtract(2,3) == -1
+    def test_subtract_two_integers_can_return_negative_value(self):
+        assert cal.subtract(2,3) == -1
 
-def test_subtract_float_and_integer_can_return_float():
-    result = cal.subtract(2.0, 1)
-    assert isinstance(result,float), "{} is not a float".format(result)
-    assert result == 1.0
+    def test_subtract_float_and_integer_can_return_float(self):
+        result = cal.subtract(2.0, 1)
+        assert isinstance(result,float), "{} is not a float".format(result)
+        assert result == 1.0
 
 # multiplication
-def test_multiply_two_integers_return_expected_value():
-    assert cal.multiply(2,2) == 4
+class TestMultiply(object):
+    def test_multiply_two_integers_return_expected_value(self):
+        assert cal.multiply(2,2) == 4
 
-def test_multiply_integer_to_string_return_none():
-    assert cal.multiply(1,'m') is None
-    
-def test_multiply_two_strings_return_none():
-    assert cal.multiply('m','i') is None
+    def test_multiply_integer_to_string_return_none(self):
+        assert cal.multiply(1,'m') is None
+        
+    def test_multiply_two_strings_return_none(self):
+        assert cal.multiply('m','i') is None
